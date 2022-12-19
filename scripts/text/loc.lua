3224913c37c137e644c3fbc71a4eeb1850b68e61
Grenier Celestin <celestin.grenier@hotmail.com>
2022-12-19 12:08:06 +0100

    more on loc

--- scripts/text/loc.lua
+++ scripts/text/loc.lua
@@ -10,18 +10,79 @@ local loc = {}
 ---@param text string?
 ---@return location
 function loc.new(line, column, text, offset)
+  assert(not line or column, "line given but not column")
   local self = {line= line or 0, column= column or 0}
 
-  local bound, offset = text, offset or 1
+  if not offset and line and text
+    then
+      offset = 1
+      for k=1,line
+        do
+          local nl = text:find('\n', offset) or #text
+          offset = nl+1
+      end
+      offset = offset+column
+  end
+
+  local bound = text or ""
 
   local mt
   mt = {
-    __index= loc,
+    __index= function(self, k)
+      if 'number' == type(k)
+        then return bound:sub(offset+k, offset+k)
+        else return loc[k]
+      end
+    end,
+
     bind= function(text) bound = text end,
     __tostring= function() return bound end,
-    __concat= function() assert(nil, "niy") end,
-    --...
-    __bnot= function() return offset end,
+
+    __add= function(self, by) -- YYY: returns self!
+      local at, to = offset, offset+by
+      local s = bound:sub(at, to-1)
+      local nl = s:find('\n')
+      while nl
+        do
+          by, s = by-nl, s:sub(nl+1)
+          self.line = self.line+1
+          self.column = 0
+          nl = s:find('\n')
+      end
+      self.column = self.column+by
+      offset = to
+      return self
+    end,
+
+    __sub= function(self, by) -- YYY: returns self!
+      local to, at = offset-by, offset
+      local s = bound:sub(to, at)
+      local nl = s:find('\n')
+      if nl
+        then
+          self.column = nl
+          self.line = s:gsub('\n', ' ')
+        else
+          self.column = self.column-by
+      end
+      offset = to
+      return self
+    end,
+
+    __concat= function(self_or_txt, txt_or_self) -- YYY: returns self!
+      if 'table' == type(self_or_txt)
+        then
+          local self, txt = self_or_txt, txt_or_self
+          bound = bound:sub(1, offset-1)..txt..bound:sub(offset)
+          return self+#txt
+        else
+          local self, txt = txt_or_self, self_or_txt
+          bound = bound:sub(1, offset-1)..txt..bound:sub(offset)
+          return self
+      end
+    end,
+
+    __bnot= function() return bound:sub(offset) end, -- YYY
     __unm= function() return offset end,
   }
 
@@ -49,5 +110,24 @@ function loc.bind(self, text)
   getmetatable(self).bind(text)
   return self
 end
+
+---@param self location
+---@return string
+function loc.repr(self)
+  return self.line..':'..self.column
+end
+
+function _playground()
+  text = [[this is the first line
+now there is the second line
+then that's the thrid and last line
+]]
+  local self = loc.new(2, 9, text)
+  print(self:repr().." _"..~self.."_")
+  self = self.."coucou"
+  print('___'..tostring(self)..'___')
+  self = self-4
+  print(self:repr().." _"..~self.."_")
+end _playground()
 
 return loc
