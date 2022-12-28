8b18295907e1026a6012da90aeb4d16147913ab7
Grenier Celestin <celestin.grenier@hotmail.com>
2022-12-28 13:37:50 +0100

    README.txt

--- scripts/impls/11.lua
+++ scripts/impls/11.lua
@@ -1,6 +1,6 @@
 local common = require 'scripts/impls/common'
 
-return common.new(function(txt)
-  print(type(txt))
+return common.new("11", function(log, txt)
+  log(type(txt))
   assert(nil, 'niy')
 end)
